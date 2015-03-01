69e18adc0988aadb7d6e364577b5d18c7f365376
dasdom <dom@dasdom.de>
2015-03-01 10:06:14 +0100

    Add version that works on Xcode 6.1.1

---
+++ ADNAPIDemoPlayground_6_1_1.playground/section-1.swift
@@ -0,0 +1,43 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+import XCPlayground
+
+let urlString = "https://api.app.net/" + "posts/stream/global"
+
+let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+
+let urlRequest = NSMutableURLRequest(URL: NSURL(string: urlString)!)
+urlRequest.HTTPMethod = "GET"
+urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+/*:
+# Create a sesseion
+
+
+*/
+let sessionTask = session.dataTaskWithRequest(urlRequest) { (data, response, error) -> () in
+  
+  let responseString = NSString(data: data, encoding: NSUTF8StringEncoding)
+  
+  var jsonError: NSError? = nil
+  let dictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &jsonError) as NSDictionary
+  //    println("dictionary: \(dictionary)")
+  let data = dictionary["data"] as [AnyObject]
+  //    println("data: \(data)")
+  for post in data {
+    //        println("post: \(post)")
+    if let text = post["text"] as? String {
+      if let user = post["user"] as? [String:AnyObject] {
+        if let username = user["username"] as? String {
+          println("*** @\(username) **********")
+          println("\(text)\n\n")
+        }
+      }
+    }
+  }
+}
+
+XCPSetExecutionShouldContinueIndefinitely(continueIndefinitely: true)
+
+sessionTask.resume()
